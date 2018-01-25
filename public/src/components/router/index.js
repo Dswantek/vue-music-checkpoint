9f47e97c9ec67908d6e0790af12c86ead8bd2dde
Dan Swantek <dswantek208@gmail.com>
2018-01-25 09:25:11 -0700

    commented out router index.js

--- public/src/components/router/index.js
+++ public/src/components/router/index.js
@@ -1,29 +1,29 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import Home from '@/components/Home'
-import SongList from '@/components/SongList'
-import MyPlaylist from '@/components/MyPlaylist'
+// import Vue from 'vue'
+// import Router from 'vue-router'
+// import Home from '@/components/Home'
+// import SongList from '@/components/SongList'
+// import MyPlaylist from '@/components/MyPlaylist'
 
 
 
-Vue.use(Router)
+// Vue.use(Router)
 
-export default new Router({
-    routes: [
-        {
-            path: '/',
-            name: 'Gramm Jamm',
-            component: Home
-        },
-        {
-            path: '/searchResults',
-            name: 'SongList',
-            component: SongList
-        },
-        {
-            path: '/playlists',
-            name: 'MyPlaylist',
-            component: MyPlaylist
-        }
-    ]
-})
+// export default new Router({
+//     routes: [
+//         {
+//             path: '/',
+//             name: 'Home',
+//             component: Home
+//         },
+//         {
+//             path: '/searchResults',
+//             name: 'SongList',
+//             component: SongList
+//         },
+//         {
+//             path: '/playlists',
+//             name: 'MyPlaylist',
+//             component: MyPlaylist
+//         }
+//     ]
+// })
